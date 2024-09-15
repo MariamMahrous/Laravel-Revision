e9c887c9d4ad6fc2bd40237201ead0d4663efcab
Mariam Mahrous <mariammahrous80@yahoo.com>
2024-09-15 21:54:21 +0300

    laravel task schedular

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+       \App\Console\Commands\Expiration::class,
     ];
 
     /**
@@ -25,7 +25,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')
-        //          ->hourly();
+        //          ->hourly(); 
+         $schedule->command('user:expire')
+                 ->everyMinute(); 
     }
 
     /**
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,8 +38,11 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    // public function username()
-    // {
-    //     return 'mobile';
-    // }
+    public function username()
+    {
+        $value=request()->input('identify');
+        $field=filter_var($value,FILTER_VALIDATE_EMAIL)?'email':'mobile';
+       request()->merge([$field=>$value]);
+       return $field;
+    }
 }
